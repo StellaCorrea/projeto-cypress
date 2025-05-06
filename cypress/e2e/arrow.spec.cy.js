94c168b66c01763c9e11fca48e4c5afc2b80d862
StellaCorrea <ster_muniz@hotmail.com>
2025-05-06 12:20:40 -0300

    exemplo completo arrow

--- cypress/e2e/arrow.spec.cy.js
+++ cypress/e2e/arrow.spec.cy.js
@@ -12,8 +12,27 @@ it('nada agora', function () { })
 
 // Aqui o exemplo da arrow function 
 
-const soma = (a, b) => {
-    return a + b
+// const soma = (a, b) => {
+//     return a + b
+// }
 
-}
+// const soma = (a, b) => a + b
+
+// const soma = (a, b) => {
+//     return a + b
+// }
+
+// const soma = (a) => a + a
+
+// const soma = a => a + a
+
+const soma = () => 5 + 5
 console.log(soma(1, 4))
+
+it('a fuction test...', function () {
+    console.log('Function', this)
+})
+
+it('an arrow test...', () => {
+    console.log('Arrow', this)
+})
